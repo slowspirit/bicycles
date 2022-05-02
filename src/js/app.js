17bcf939b161ac038ceb25724457eb14e016fdf8
slowspirit <slowspirit@gmail.com>
2022-05-02 13:35:30 +0300

    added second and third brands sections and slider section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 // import * as flsFunctions from "./modules/functions.js"
 import ibg from "./modules/ibg.js"
+import slider from "./modules/slider.js"
 import headerScripts from "./modules/header.js"
 import inputNumber from "./modules/inputNumber.js";
 import pagination from "./modules/pagination.js";
@@ -9,6 +10,7 @@ import accordion from "./modules/accordion.js";
 // flsFunctions.isWebp();
 ibg();
 headerScripts();
-inputNumber();
-pagination();
-accordion();
+slider();
+// inputNumber();
+// pagination();
+// accordion();
---
+++ src/js/modules/slider.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+import slick from "slick-carousel";
+
+export default function () {
+    $('.slider__body').slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+
+                }
+            }
+        ]
+    });
+}
